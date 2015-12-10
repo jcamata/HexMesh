07080753f567e24f675102c26643bc4df7b5e8f0
Régis Cottereau <cottereau@MacBook-Pro-de-Regis.local>
2015-12-10 14:02:21 -0200

    [ADD] new model (hawai)

--- matlab/mainSRTM.m
+++ matlab/mainSRTM.m
@@ -4,9 +4,10 @@ clear all
 % set bounds (in degrees and minutes)
 % [ minlat° minlat' maxlat° maxlat' minlon° minlon' maxlon° maxlon']
 % l = [40 00 52 00  -5 00   8 00]; % France
-l = [43 48 44 05   4 30   5 00]; % Cadarache, France
-% l = [38 00 38 35  20 00  21 00]; % Kefalonia, Greece
+% l = [43 48 44 05   4 30   5 00]; % Cadarache, France
+l = [38 00 39 00  20 00  21 00]; % Kefalonia, Greece
 % l = [37 10 37 40 138 15 138 55]; % Kashiwazaki, Japan
+% l = [18 00 21 50 -158 50 -154 00]; % Mauna Loa, Hawai
 
 % choose output directory
 outdir = '.';
