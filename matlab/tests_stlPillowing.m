40e486060d3006097844e582f5365d351b4e3850
labcorrea <labcorrea@gmail.com>
2019-05-24 14:14:51 -0300

    [ADD] more tests in stl cuts

--- matlab/tests_stlPillowing.m
+++ matlab/tests_stlPillowing.m
@@ -2,11 +2,35 @@ close all
 clear variables
 clc
 
-teste1 = 0;
-teste2 = 1;
-teste3 = 0;
+teste1 = 0; %z
+teste2 = 0; %x
+teste3 = 0; %y
+
+% cortes tipo "2"
 teste4 = 0;
 teste5 = 0;
+teste6 = 0;
+teste7 = 0;
+
+%cortes tipo "3"
+teste8  = 1;
+teste9  = 0;
+teste10 = 0;
+teste11 = 0;
+teste12  = 0;
+teste13 = 0;
+teste14 = 0;
+teste15 = 0;
+
+%cortes tipo "4"
+teste16  = 0;
+teste17 = 0;
+teste18 = 0;
+teste19 = 0;
+teste20  = 0;
+teste21  = 0;
+teste22 = 0;
+teste23 = 0;
 
 % set the path for wget function
 wget_path = '/opt/local/bin';
@@ -31,6 +55,13 @@ if(teste1)
     y = [0 1]*5;
     z = [1 1; 1 1]*1.5;
     write_stl([output_name,'_bathy.stl'],x,y,z)
+    
+    system(['dos2unix ',output_name,'_topo.stl']);
+    system(['dos2unix ',output_name,'_bathy.stl']);
+    system(['stl2gts <',output_name,'_topo.stl > ../input/',output_name,'_topo.gts']);
+    system(['stl2gts <',output_name,'_bathy.stl > ../input/',output_name,'_bathy.gts']);
+    system(['mv ',output_name,'_topo.stl ../input/.']);
+    system(['mv ',output_name,'_bathy.stl ../input/.']);
 end
 
 
@@ -49,6 +80,13 @@ if(teste2)
     y = [0 1]*5;
     z = [-1 1; -1 1]*10;
     write_stl([output_name,'_bathy.stl'],x,y,z)
+    
+    system(['dos2unix ',output_name,'_topo.stl']);
+    system(['dos2unix ',output_name,'_bathy.stl']);
+    system(['stl2gts <',output_name,'_topo.stl > ../input/',output_name,'_topo.gts']);
+    system(['stl2gts <',output_name,'_bathy.stl > ../input/',output_name,'_bathy.gts']);
+    system(['mv ',output_name,'_topo.stl ../input/.']);
+    system(['mv ',output_name,'_bathy.stl ../input/.']);
 end
 %% test 3
 if(teste3)
@@ -65,7 +103,159 @@ if(teste3)
     y = [1 1]*1;
     z = [-1 -1; 1 1]*10;
     write_stl([output_name,'_bathy.stl'],x,y,z)
+    
+    system(['dos2unix ',output_name,'_topo.stl']);
+    system(['dos2unix ',output_name,'_bathy.stl']);
+    system(['stl2gts <',output_name,'_topo.stl > ../input/',output_name,'_topo.gts']);
+    system(['stl2gts <',output_name,'_bathy.stl > ../input/',output_name,'_bathy.gts']);
+    system(['mv ',output_name,'_topo.stl ../input/.']);
+    system(['mv ',output_name,'_bathy.stl ../input/.']);
 end
+%% test 4
+if(teste4)
+    output_name = 'teste4';
+    
+    %topo
+    x = [0 1]*5;
+    y = [0 1]*5;
+    z = [2 2; 3 3];
+    write_stl([output_name,'_topo.stl'],x,y,z)
+    
+    %bathy
+    x = [0 1]*5;
+    y = [0 1]*5;
+    z = [-5 -5; 1.1 1.1]*1.5;
+    write_stl([output_name,'_bathy.stl'],x,y,z)
+    
+    system(['dos2unix ',output_name,'_topo.stl']);
+    system(['dos2unix ',output_name,'_bathy.stl']);
+    system(['stl2gts <',output_name,'_topo.stl > ../input/',output_name,'_topo.gts']);
+    system(['stl2gts <',output_name,'_bathy.stl > ../input/',output_name,'_bathy.gts']);
+    system(['mv ',output_name,'_topo.stl ../input/.']);
+    system(['mv ',output_name,'_bathy.stl ../input/.']);
+end
+%% test 5
+if(teste5)
+    output_name = 'teste5';
+    
+    %topo
+    x = [0 1]*5;
+    y = [0 1]*5;
+    z = [2 2; 3 3];
+    write_stl([output_name,'_topo.stl'],x,y,z)
+    
+    %bathy
+    x = [0 1]*5;
+    y = [0 1]*5;
+    z = [-5 1.1; -5 1.1]*1.5;
+    write_stl([output_name,'_bathy.stl'],x,y,z)
+    
+    system(['dos2unix ',output_name,'_topo.stl']);
+    system(['dos2unix ',output_name,'_bathy.stl']);
+    system(['stl2gts <',output_name,'_topo.stl > ../input/',output_name,'_topo.gts']);
+    system(['stl2gts <',output_name,'_bathy.stl > ../input/',output_name,'_bathy.gts']);
+    system(['mv ',output_name,'_topo.stl ../input/.']);
+    system(['mv ',output_name,'_bathy.stl ../input/.']);
+end
+%% test 6
+if(teste6)
+    output_name = 'teste6';
+    
+    %topo
+    x = [0 1]*5;
+    y = [0 1]*5;
+    z = [2 2; 3 3];
+    write_stl([output_name,'_topo.stl'],x,y,z)
+    
+    %bathy
+    x = [0 1]*5;
+    y = [0.9 0]*5;
+    z = [-5 -5; 1.1 1.1]*1.5;
+    write_stl([output_name,'_bathy.stl'],x,y,z)
+    
+    system(['dos2unix ',output_name,'_topo.stl']);
+    system(['dos2unix ',output_name,'_bathy.stl']);
+    system(['stl2gts <',output_name,'_topo.stl > ../input/',output_name,'_topo.gts']);
+    system(['stl2gts <',output_name,'_bathy.stl > ../input/',output_name,'_bathy.gts']);
+    system(['mv ',output_name,'_topo.stl ../input/.']);
+    system(['mv ',output_name,'_bathy.stl ../input/.']);
+end
+%% test 7
+if(teste7)
+    output_name = 'teste7';
+    
+    %topo
+    x = [0 1]*5;
+    y = [0 1]*5;
+    z = [2 2; 3 3];
+    write_stl([output_name,'_topo.stl'],x,y,z)
+    
+    %bathy
+    x = [1 0]*5;
+    y = [0 1]*5;
+    z = [-5 1.1; -5 1.1]*1.5;
+    write_stl([output_name,'_bathy.stl'],x,y,z)
+    
+    system(['dos2unix ',output_name,'_topo.stl']);
+    system(['dos2unix ',output_name,'_bathy.stl']);
+    system(['stl2gts <',output_name,'_topo.stl > ../input/',output_name,'_topo.gts']);
+    system(['stl2gts <',output_name,'_bathy.stl > ../input/',output_name,'_bathy.gts']);
+    system(['mv ',output_name,'_topo.stl ../input/.']);
+    system(['mv ',output_name,'_bathy.stl ../input/.']);
+end
+%% test 8
+if(teste8)
+    output_name = 'teste8';
+    
+    %topo
+    x = [0 1]*5;
+    y = [0 1]*5;
+    z = [2 2; 3 3];
+    write_stl([output_name,'_topo.stl'],x,y,z)
+    
+    %bathy
+    x = [0 1]*5;
+    y = [-0.5 0.5]*5;
+    z = [-7 -5; -3 1.1]*1.5+8;
+    write_stl([output_name,'_bathy.stl'],x,y,z)
+
+    system(['dos2unix ',output_name,'_topo.stl']);
+    system(['dos2unix ',output_name,'_bathy.stl']);
+    system(['stl2gts <',output_name,'_topo.stl > ../input/',output_name,'_topo.gts']);
+    system(['stl2gts <',output_name,'_bathy.stl > ../input/',output_name,'_bathy.gts']);
+    system(['mv ',output_name,'_topo.stl ../input/.']);
+    system(['mv ',output_name,'_bathy.stl ../input/.']);
+end
+%% test 9
+if(teste9)
+    output_name = 'teste9';
+    
+    %topo
+    x = [0 1]*5;
+    y = [0 1]*5;
+    z = [2 2; 3 3];
+    write_stl([output_name,'_topo.stl'],x,y,z)
+    
+    %bathy
+    x = [1 0]*5;
+    y = [0 1]*5;
+    z = [-5 1.1; -5 1.1]*1.5;
+    write_stl([output_name,'_bathy.stl'],x,y,z)
+    
+    system(['dos2unix ',output_name,'_topo.stl']);
+    system(['dos2unix ',output_name,'_bathy.stl']);
+    system(['stl2gts <',output_name,'_topo.stl > ../input/',output_name,'_topo.gts']);
+    system(['stl2gts <',output_name,'_bathy.stl > ../input/',output_name,'_bathy.gts']);
+    system(['mv ',output_name,'_topo.stl ../input/.']);
+    system(['mv ',output_name,'_bathy.stl ../input/.']);
+end
+%%
+
+
+
+
+
+return
 %% test 4
 if(teste4)
     output_name = 'teste4';
@@ -82,28 +272,3 @@ if(teste4)
     z = [-6 -6 -6 -6; 1 1 1 1; 1 1 1 1; 1.1 1.1 1.1 1.1]*1.5;
     write_stl([output_name,'_bathy.stl'],x,y,z)
 end
-%% test 5
-if(teste5)
-    output_name = 'teste5';
-    
-    %topo
-    x = [0 1]*5;
-    y = [0 1]*5;
-    z = [2 2; 3 3];
-    write_stl([output_name,'_topo.stl'],x,y,z)
-    
-    %bathy
-    x = [0 1]*5;
-    y = [0 1]*5;
-    z = [-5 -5; 1.1 1.1]*1.5;
-    write_stl([output_name,'_bathy.stl'],x,y,z)
-end
-
-
-%%
-system(['dos2unix ',output_name,'_topo.stl']);
-system(['dos2unix ',output_name,'_bathy.stl']);
-system(['stl2gts <',output_name,'_topo.stl > ../input/',output_name,'_topo.gts']);
-system(['stl2gts <',output_name,'_bathy.stl > ../input/',output_name,'_bathy.gts']);
-system(['mv ',output_name,'_topo.stl ../input/.']);
-system(['mv ',output_name,'_bathy.stl ../input/.']);
